162e059ccffa787c1d84ca5b3774ab5bf23e55ef
Team-Lightning-LLC <lehmanachris@gmail.com>
2025-12-23 18:33:04 -0500

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-// Haven App - Main exports
-export { default as App } from './App';
-export { default as HomePage } from './HomePage';
-export { default as OnboardingFlow } from './OnboardingFlow';
-export { default as styles } from './styles';
